fd3c509bf441190285020e771808f7c94951d8b0
mauroxcf <maurox03@gmail.com>
2020-06-16 05:03:15 +0000

    print alphabet in lower and uppercase with a conditional command

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+char b;
+char c;
+
+for (b = 'a'; b <= 'z'; b++)
+{
+putchar (b);
+}
+
+for (c = 'A'; c <= 'Z'; c++)
+{
+putchar (c);
+}
+putchar ('\n');
+
+return (0);
+
+}
