ad415511dc7f10cb812b07595bee2d2c753221c4
mauroxcf <maurox03@gmail.com>
2020-06-16 00:47:17 +0000

    print last digit of the number using conditional statement

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,9 +19,9 @@ n = rand() - RAND_MAX / 2;
 m = n % 10;
 printf("Last digit of %d is %d ", n, m);
 
-if (m > 5)
+if (m == 0)
 {
-printf("and is greater than 5\n");
+printf("and is 0\n");
 }
 
 else if (m < 6)
@@ -31,7 +31,7 @@ printf("and is less than 6 and not 0\n");
 
 else
 {
-printf("and is 0\n");
+printf("and is greater than 5\n");
 }
 return (0);
 }
