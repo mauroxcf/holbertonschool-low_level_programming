6d1dbae2a60ffe80e9919f560d202d3442bc3b2d
mauroxcf <maurox03@gmail.com>
2020-06-19 06:49:14 +0000

    program with a function that prints 10 times the numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,7 +1,7 @@
 #include "holberton.h"
 
 /**
- * print_more_numbers - print more numbers with putchar.
+ * _more_numbers - print more numbers with putchar.
  * _putchar: print characters.
  */
 
@@ -17,7 +17,7 @@ for (b = 0; b <= 14; b++)
 {
 if (b / 10 >= 1)
 {
-_putchar ( b / 10 + '0');
+_putchar (b / 10 + '0');
 }
 
 _putchar (b % 10 + '0');
