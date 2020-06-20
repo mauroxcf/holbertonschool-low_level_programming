fd6f1ad0edf353efbe4c59e7371d224ffd389ef0
mauroxcf <maurox03@gmail.com>
2020-06-20 20:36:24 +0000

    program that prints backslash a numbers of time

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,27 +1,24 @@
 #include "holberton.h"
 
 /**
- * print_diagonal - print diagonal lines .
- * @n: variable
- * @m: numbers of times thats gonna affect the lines or spaces
- * @p: numbers of spaces
+ * print_diagonal - print diagonal lines.
+ * @n: variable.
  * Return: Always 0.
  */
 
 void print_diagonal(int n)
 {
 
-  int m, p;
+int m, p;
 
-  for (m = 0; m < n; m++)
-    {
-      for (p = 0; p < m; p++)
-	
-	  _putchar (32);
-	  _putchar (92);
-	  _putchar ('\n');
-	}
-  if (n < 0)   
-  _putchar ('\n');
-    }
+for (m = 0; m < n; m++)
+{
+for (p = 0; p < m; p++)
 
+_putchar (32);
+_putchar (92);
+_putchar ('\n');
+}
+if (n < 0)
+_putchar ('\n');
+}
