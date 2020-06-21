e3c43a4f66310b4cc3787086b084c2b5c9367775
mauroxcf <maurox03@gmail.com>
2020-06-21 01:49:03 +0000

    program that prints the numbers 1 to 100 with the Fizz-Buzz test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,17 +12,17 @@ int n;
 for (n = 1; n <= 100; n++)
 {
 if (n % 15 == 0)
-
+{
 printf("FizzBuzz ");
-
+}
 else if (n % 3 == 0)
-
+{
 printf("Fizz ");
-
+}
 else if (n % 5 == 0)
-
+{
 printf("Buzz ");
-
+}
 else
 printf("%d ", n);
 }
