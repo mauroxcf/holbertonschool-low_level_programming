0d93790071c2b1078653d347e205ca577283af1c
mauroxcf <maurox03@gmail.com>
2020-06-21 01:42:54 +0000

    program that prints the numbers 1 to 100 with the Fizz-Buzz test

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+int n;
+for (n = 1; n <= 100; n++)
+{
+if (n % 15 == 0)
+
+printf("FizzBuzz ");
+
+else if (n % 3 == 0)
+
+printf("Fizz ");
+
+else if (n % 5 == 0)
+
+printf("Buzz ");
+
+else
+printf("%d ", n);
+}
+printf("\n");
+return (0);
+}
