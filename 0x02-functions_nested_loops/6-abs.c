c3a18a6e315a306e7dc69a556bdef0ebca5c5910
mauroxcf <maurox03@gmail.com>
2020-06-22 05:33:15 +0000

    program that computes the absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * _abs - prints the value of a interger.
+ * Return: Always 0.
+ */
+
+
+int _abs(int a)
+{
+  
+if (a >= 0)
+{
+return (a);
+}
+else
+{
+return (0 - a);
+}
+return (0);
+}
