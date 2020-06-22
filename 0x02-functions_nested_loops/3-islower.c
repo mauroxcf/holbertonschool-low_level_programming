14b094d39dad23e64195dbd89b5300bea9d79810
mauroxcf <maurox03@gmail.com>
2020-06-22 03:49:37 +0000

    program that print 1 if a letter its in lowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet_x10 - print the alphabet ten times.
+ *
+ * Return: Always 0.
+ */
+
+
+int _islower(int c)
+{
+
+  if ( c >= 'a' && c <= 'z')
+    {   
+ return (1); 
+    }
+  else
+    {    
+return (0);
+    }
+}
