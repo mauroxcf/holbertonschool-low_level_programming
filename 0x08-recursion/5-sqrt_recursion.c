ce59f5bb9cc70e06545fa24f6fe6f49af310d97c
mauroxcf <maurox03@gmail.com>
2020-07-04 06:31:54 +0000

    function that returns the natural square root of a number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,7 +1,6 @@
 #include "holberton.h"
 
 /**
- * _sqrt_recursion - factorial in recursion.
  * square_checker - counter the iteracions of root square numbers.
  * @n: variable natural number.
  * @counter: counter the interation.
@@ -22,6 +21,11 @@ return (-1);
 return (square_checker(n, counter + 1));
 }
 
+/**
+ * _sqrt_recursion - function that return square root number.
+ * @n: variable natural number.
+ * Return: Always 0.
+ */
 
 int _sqrt_recursion(int n)
 {
