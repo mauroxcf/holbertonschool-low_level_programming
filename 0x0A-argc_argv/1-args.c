e086884de75d530ed39edff296bac14dfa6f93bb
mauroxcf <maurox03@gmail.com>
2020-07-05 04:43:19 +0000

    program that prints the number of arguments passed

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code for Holberton School students.
+ * @argc:counter
+ * @argv:pointer string array
+ * Return: Always 0.
+ */
+
+
+int main(int argc, char *argv[] __attribute__((unused)))
+{
+printf("%d\n", argc -1);
+
+return (0);
+
+}
+
+
+
+
---
+++ 0x0A-argc_argv/1-args.c~
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code for Holberton School students.
+ * @argc:counter
+ * @argv:pointer string array
+ * Return: Always 0.
+ */
+
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
