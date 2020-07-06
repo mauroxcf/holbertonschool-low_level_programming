4a8d25d4f088980af899c6a36e3f1cbb9766453d
mauroxcf <maurox03@gmail.com>
2020-07-06 02:28:44 +0000

    program that multiplies two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code for Holberton School students.
+ * @argc:counter
+ * @argv:pointer string array
+ * Return: Always 0.
+ */
+
+
+int main(int argc, char *argv[])
+{
+
+
+int result;
+
+if (argc != 3)
+{
+printf("Error");
+return (1);
+}
+
+
+argv++;
+result = atoi(*argv);
+argv++;
+result *= atoi(*argv);
+
+printf("%d\n", result);
+
+
+
+return (0);
+}
+
+
+
+
