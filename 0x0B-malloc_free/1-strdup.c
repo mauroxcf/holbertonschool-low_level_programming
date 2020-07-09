989b1ce6b55329e6dbc8ae52f961832a5f38896b
mauroxcf <maurox03@gmail.com>
2020-07-09 04:08:02 +0000

    function that returns a pointer to a newly allocated

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -21,7 +21,7 @@ if (!str)
 return (NULL);
 }
 for (a = 0; str[a] != '\0'; a++)
-;
+{
 
 ptr = malloc(sizeof(char) * a);
 
@@ -29,11 +29,12 @@ if (!str)
 {
 return (NULL);
 }
+}
 
 for (b = 0; str[b]; b++)
-  {
+{
 ptr[b] = str[b];
-  }
+}
 
 return (ptr);
 }
