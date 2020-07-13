28c489c14ceddcbf7516445b71bbcc6598be2284
mauroxcf <maurox03@gmail.com>
2020-07-13 06:25:18 +0000

    function that capitalizes all words of a string

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+B#include "holberton.h"
 #include <stdio.h>
 
 /**
@@ -12,15 +12,20 @@ char *cap_string(char *ptr)
 
 int a;
 int b;
-char s[] = {9, 10 , 32, 33, 34, 40, 41, 44, 46, 59, 63, 123, 125};
+char *s;
+s = " \t\n,;.!\?\"(){}";
 
 for (a = 0; ptr[a] != '\0'; a++)
 {
 for (b = 0; s[b] != '\0'; b++)
 {
 if (s[b] == ptr[a])
+{
 if (ptr[a + 1] >= 'a' && ptr[a + 1] <= 'z')
+{
 ptr[a + 1] = ptr[a + 1] - 'a' + 'A';
+}
+}
 }
 }
 return (ptr);
