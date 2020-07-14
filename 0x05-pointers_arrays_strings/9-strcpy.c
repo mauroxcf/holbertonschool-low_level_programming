b1c957395aa63c5a3e07454590fd54a294dd594e
mauroxcf <maurox03@gmail.com>
2020-07-14 04:29:33 +0000

    function that copies the string pointed

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * _strcpy(char *dest, char *src) - output of characters and strings.
+ * *_strcpy - output of characters and strings.
  * @dest:pointer variable.
  * @src:varibale
  * Return: Always 0.
@@ -10,3 +10,16 @@
 
 char *_strcpy(char *dest, char *src)
 {
+
+int count;
+
+
+for (count = 0; src[count] != '\0'; count++)
+
+dest[count] = src[count];
+
+
+dest[count] = '\0';
+
+return (dest);
+}
