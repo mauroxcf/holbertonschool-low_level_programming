492c1590ebfb415be98a5d104a8391f2a0d0d761
mauroxcf <maurox03@gmail.com>
2020-07-14 22:07:28 +0000

    function that encodes a string using rot13

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -2,33 +2,33 @@
 #include <stdio.h>
 
 /**
- * main - check the code for Holberton School students.
- *
+ * *rot13 - move each word in 13 positions of the string.
+ *@s:string of characters.
  * Return: Always 0.
  */
 
 char *rot13(char *s)
 {
 
-  int count;
-  int count2;
-  char *alpha;
-  char *rot13;
- 
-  alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-  rot13 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+int count;
+int count2;
+char *alpha;
+char *rot13;
 
-  for (count = 0; s[count] != '\0'; count++)
-    {
-      for (count2 = 0; alpha[count2] != '\0'; count2++)
-	{
-	  if (s[count] == alpha[count2])
-	    {
-	    s[count] = rot13[count2];
-	    break;
-	    }
-	}
-    }
+alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+rot13 = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-  return (s);
+for (count = 0; s[count] != '\0'; count++)
+{
+for (count2 = 0; alpha[count2] != '\0'; count2++)
+{
+if (s[count] == alpha[count2])
+{
+s[count] = rot13[count2];
+break;
+}
+}
+}
+
+return (s);
 }
