be92bdce69a5f32bed33637f4aebc2026a5cb26c
mauroxcf <maurox03@gmail.com>
2020-07-15 15:37:52 +0000

    function that copies memory area

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,13 +12,17 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 
-unsigned int count;
+  unsigned int count;
+  unsigned int count2;
 
-
-for (count = 0; src[count] < src[n]; count++)
-{
-dest[count] = src[count];
-}
-return (dest);
+  for (count = 0; count < n ; count++)
+    {
+      for (count2 = 0; count2 < n; count2++)
+        {
+	  dest[count] = src[count];
+        }
+    }
+    
+  return (dest);
 
 }
