1b3f72b62a3807edcfcd268594a4955323361cdd
mauroxcf <maurox03@gmail.com>
2020-07-18 17:19:17 +0000

    function that prints a struct dog

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -13,11 +13,11 @@ void print_dog(struct dog *d)
 	{
 		return;
 	}
-	if ((*d).name == NULL)
+	else if ((*d).name == NULL)
 	{
 		printf("Name: (nil)\n");
 	}
-	if ((*d).owner == NULL)
+	else if ((*d).owner == NULL)
 	{
 		printf("Owner: (nil)\n");
 	}
