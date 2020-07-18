afb88527c3736d0cd3fdfadb734dcdc2357cf9d3
mauroxcf <maurox03@gmail.com>
2020-07-18 05:20:56 +0000

    Define a new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - struct.
+ *@name: dog name.
+ *@age: dog age.
+ *@owner: dog owner.
+ */
+
+struct dog
+{
+
+	char *name;
+	float age;
+	char *owner;
+
+};
+
+#endif
