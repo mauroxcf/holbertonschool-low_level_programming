26a98fcb8a1ae1bf6bfff2180e7e9230071c3587
mauroxcf <maurox03@gmail.com>
2020-07-23 00:07:07 +0000

    Write a function that creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,44 @@
 #include <stdlib.h>
 #include "dog.h"
 
+
+/**
+ * *_strdup - prints buffer in hexa
+ * @str: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+
+char *_strdup(char *str)
+{
+
+	int a;
+	char *ptr;
+	int b;
+
+
+	if (!str)
+	{
+		return (NULL);
+	}
+	for (a = 0; str[a] != '\0'; a++)
+		;
+
+	ptr = malloc(sizeof(char) * (a + 1));
+
+	if (!ptr)
+	{
+		return (NULL);
+	}
+
+	for (b = 0; str[b] != '\0'; b++)
+	{
+		ptr[b] = str[b];
+	}
+	ptr[b] = '\0';
+	return (ptr);
+}
+
 /**
  * new_dog - new dog.
  *@name: name of the dog.
@@ -10,19 +48,26 @@
  * Return: the new dog objects.
  */
 
+
 dog_t *new_dog(char *name, float age, char *owner)
 {
+
+
+
+
 	dog_t *newdog;
 
 	newdog = malloc(sizeof(struct dog));
+
+
 
 	if (newdog == NULL)
 	{
 		return (NULL);
 	}
-	newdog->name = name;
+	newdog->name = _strdup(name);
 	newdog->age = age;
-	newdog->owner = owner;
+	newdog->owner = _strdup(owner);
 
 	return (newdog);
 }
