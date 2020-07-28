819906b378f273cb4b974450248af75765228f5f
mauroxcf <maurox03@gmail.com>
2020-07-28 06:04:46 +0000

    function that returns a pointer to a 2 dimensional array of integers

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid - free the grid memori spaces
+ * @grid: the address of the two dimensional grid
+ * @height: height of the grid
+ *
+ * Return: Nothing.
+ */
+
+void free_grid(int **grid, int height)
+{
+	int a;
+
+	for (a = 0; a < height; a++)
+	{
+		free(grid[a]);
+	}
+
+}
