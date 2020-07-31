8976ad82aaf9c915f54575fad3894fd0699c64bd
mauroxcf <maurox03@gmail.com>
2020-07-31 03:56:11 +0000

    function that converts a binary number

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * binary_to_uint - converts a binary number.
+ *@b: string.
+ * Return: Always 0.
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int a = 0;
+	int result = 0;
+	int pow = 1;
+
+	while (b[a] != 0)
+	{
+		a++;
+	}
+	a--;
+
+	while (a >= 0)
+	{
+		if (b[a] != '1' && b[a] != '0')
+		{
+			return (0);
+		}
+		result += (b[a] - 48) * pow;
+		pow *= 2;
+		a--;
+	}
+	return (result);
+}
