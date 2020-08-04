392122b72d84304eb647e9750c7585af535b0f86
mauroxcf <maurox03@gmail.com>
2020-08-04 05:21:20 +0000

    function that prints before the main function is executed

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+ #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+/**
+ *before_main - print before main
+ *Return: return the number of nodes
+ */
+
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
