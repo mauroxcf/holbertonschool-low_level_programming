33a9b5736cb4098bc274dad88f90a4e5e2fa11ad
mauroxcf <maurox03@gmail.com>
2020-10-03 02:08:39 +0000

    Write a hash function implementing the djb2 algorithm

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "hash_tables.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+hash_table_t *hash_table_create(unsigned long int size);
+{
+
+}
