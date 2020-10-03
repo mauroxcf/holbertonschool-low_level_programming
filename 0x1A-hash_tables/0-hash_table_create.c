0a9a9863cafc2649ad7c8356a8dd96b771271e2d
mauroxcf <maurox03@gmail.com>
2020-10-03 02:11:30 +0000

    Write a function that creates a hash table.

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,14 +1,28 @@
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "hash_tables.h"
 
 /**
- * main - check the code for Holberton School students.
+ * hash_table_create - Creates a hash table
  *
- * Return: Always EXIT_SUCCESS.
- */
-hash_table_t *hash_table_create(unsigned long int size);
+ * @size: Numbers of buckets to create
+ *
+ * Return: New hash_table or NULL if failed
+ **/
+hash_table_t *hash_table_create(unsigned long int size)
 {
+	hash_table_t *hash_table;
 
+	hash_table = calloc(1, sizeof(hash_table_t));
+	if (hash_table == NULL)
+		return (NULL);
+
+	hash_table->size = size;
+	hash_table->array = calloc(size, sizeof(hash_node_t *));
+	if (hash_table->array == NULL)
+	{
+		free(hash_table);
+		return (NULL);
+	}
+
+	return (hash_table);
 }
