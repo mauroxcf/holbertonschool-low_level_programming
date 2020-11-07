5e0dc8e5881a4bb7c8339996a79dd079e20843f8
mauroxcf <maurox03@gmail.com>
2020-11-07 00:54:38 +0000

    5. Island Perimeter other logic

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -11,12 +11,9 @@ def island_perimeter(grid):
     for x in range(len(grid)):
         for y in range(len(grid[x])):
             if grid[x][y]:
-                    if y > 0 and grid[x][y-1] == 0:
-                        perimeter += 1
-                    if y > 0 and grid[x][y+1] == 0:
-                        perimeter += 1
-                    if x > 0 and grid[x-1][y] == 0:
-                        perimeter += 1
-                    if x > 0 and grid[x+1][y] == 0:
-                        perimeter += 1
+                    perimeter += 4
+                    if x > 0 and grid[x-1][y] == 1:
+                        perimeter -= 2
+                    if y > 0 and grid[x][y-1] == 1:
+                        perimeter -= 2
     return perimeter
